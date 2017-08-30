28686500c83e5967f90997b1ee26dccc5bc1ec3d
littleseven <305844021@qq.com>
2017-08-30 14:33:33 +0800

    123

--- src/config/env.js
+++ src/config/env.js
@@ -1,13 +1,13 @@
 /**
  * 配置编译环境和线上环境之间的切换
- * 
+ *
  * baseUrl: 域名地址
  * routerMode: 路由模式
  * imgBaseUrl: 图片所在域名地址
- * 
+ *
  */
 
-let baseUrl = ''; 
+let baseUrl = '';
 let routerMode = 'history';
 let imgBaseUrl = 'http://images.cangdu.org/';
 
@@ -17,6 +17,7 @@ if (process.env.NODE_ENV == 'development') {
 }else if(process.env.NODE_ENV == 'production'){
 
 	// baseUrl = 'http://cangdu.org:8001';
+  console.log("test")
 }
 
 export {
